f6504614fc50e858a7834f43c9fa314e7b940684
Alex <david_alejandrorizo@outlook.com>
2025-08-04 16:17:55 -0600

    websockets pending

--- src/tasks-ws/tasks-ws.gateway.ts
+++ src/tasks-ws/tasks-ws.gateway.ts
@@ -1,7 +1,32 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { TasksWsService } from './tasks-ws.service';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class TasksWsGateway {
+@WebSocketGateway({
+  cors: {
+    origin: process.env.CLIENT_URL ?? 'http://localhost:5173',
+    credentials: true,
+  },
+})
+export class TasksWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
   constructor(private readonly tasksWsService: TasksWsService) {}
+
+  afterInit(server: Server) {
+    console.log('WebSocket server initialized', server.engine.opts.cors);
+  }
+
+  handleConnection(client: Socket) {
+    console.log('Client connectedddd', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected', client.id);
+  }
 }
